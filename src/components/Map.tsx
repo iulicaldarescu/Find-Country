8cfc4a205f8c4ba852aae6918b95bafc53cce782
Caldarescu Iulian <caldarescuiulian@gmail.com>
2023-12-27 11:55:41 +0200

    icon marker added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import styles from "../styles/LeafletMap.module.css";
-
+import { HiLocationMarker } from "react-icons/hi";
+import ReactDOMServer from "react-dom/server";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import styles from "../styles/LeafletMap.module.css";
+import "leaflet/dist/leaflet.css";
+
 function Map() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
@@ -13,9 +17,17 @@ function Map() {
   const lng = parseFloat(searchParams.get("lng") || "");
 
   useEffect(() => {
-    console.log(lat + " " + lng);
     setPosition([lat, lng]);
   }, [lat, lng]);
+
+  const customMarkerIcon = L.divIcon({
+    className: "custom-marker-icon",
+    html: ReactDOMServer.renderToString(
+      <HiLocationMarker size={32} color="red" />
+    ),
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+  });
 
   return (
     <MapContainer
@@ -28,6 +40,13 @@ function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+
+      {/* Place Marker if position is available */}
+      {position && (
+        <Marker position={position} icon={customMarkerIcon}>
+          {/* You can add a Popup or Tooltip here if needed */}
+        </Marker>
+      )}
     </MapContainer>
   );
 }
